396536f429e3c3f864656f624c157866aae09b7a
@a.blinov <toni.blinov.04@bk.ru>
2025-09-30 13:42:03 +0300

    basic stuff

---
+++ services/profile/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def Hi():
+    return "hello"
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(app=app, host="127.0.0.1", port=9000)
