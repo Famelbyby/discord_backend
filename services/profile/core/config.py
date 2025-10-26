02f695e3abeb9c299316909bd3ee492ca0f06fac
@a.blinov <toni.blinov.04@bk.ru>
2025-10-26 12:47:40 +0300

    путь для докера

--- services/profile/core/config.py
+++ services/profile/core/config.py
@@ -5,7 +5,7 @@ class Config(BaseSettings):
 
     db_name: str = "profile_db"
 
-    db_link: str = f"postgres:postgres@localhost:5430/{db_name}"
+    db_link: str = f"postgres:postgres@postgres:5432/{db_name}"
 
     async_db_url: str = f"postgresql+asyncpg://{db_link}"
 
